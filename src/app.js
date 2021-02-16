53adcf9358370fe8fc58fc74be978f207ccdcb65
sutter-dave <sutter@intransix.com>
2021-02-16 14:49:08 -0800

    more repo updating

--- src/app.js
+++ src/app.js
@@ -1,9 +1,16 @@
-import {apogeeutil,apogee,apogeeapp,apogeeui,apogeeview} from "./lib/apogeeAppBundle.es.js";
+import apogeeutil from "../../test-node-libs/apogeejs-util-lib/v2.0.0-p1/apogeeUtilLib.js";
+import * as apogeebase from "../../test-node-libs/apogeejs-base-lib/v2.0.0-p1/apogeeBaseLib.js";
+import * as apogee from "../../test-node-libs/apogeejs-model-lib/v2.0.0-p1/apogeeModelLib.js";
+import * as apogeeapp from "../../test-node-libs/apogeejs-app-lib/v2.0.0-p1/apogeeAppLib.js";
+import * as apogeeui from "../../test-node-libs/apogeejs-ui-lib/v2.0.0-p1/apogeeUiLib.js";
+import * as apogeeview from "../../test-node-libs/apogeejs-view-lib/v2.0.0-p1/apogeeViewLib.js";
+import {ApogeeView} from "../../test-node-libs/apogeejs-appview-lib/v2.0.0-p1/apogeeAppViewLib.js";
 import ElectronBridgeAppConfigManager from "./ElectronBridgeAppConfigManager.js";
-const { ApogeeView, initIncludePath } = apogeeview;
+const { initIncludePath } = apogeeview;
 
 //expose these apogee libraries globally so plugins can use them
 window.apogeeutil = apogeeutil;
+window.apogeebase = apogeebase;
 window.apogee = apogee;
 window.apogeeapp = apogeeapp;
 window.apogeeui = apogeeui;
